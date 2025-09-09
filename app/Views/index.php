f1d7216e506462c8274cfb8db02f61ac16c0eda2
DantoDust447 <dantejosesanchez724@gmail.com>
2025-09-09 08:16:06 -0600

    upload

---
+++ app/Views/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <title>Zenith</title>
+</head>
+
+<body>
+    <header>
+        <br>
+        <nav>
+
+            <nav class="nav">
+                <img src="<?=base_url('images/shime37.png')?>" alt="Logo" class="d-inline-block align-text-top">
+                <a class="nav-link active" aria-current="page" href="#">Active</a>
+                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#">Link</a>
+            </nav>
+        </nav>
+    </header>
+    <hr>
+</body>
+
+</html>
