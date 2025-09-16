df664962b118f415b726a655a76ffb90298cf586
DantoDust447 <dantejosesanchez724@gmail.com>
2025-09-16 10:37:12 -0600

    si

--- app/Views/index.php
+++ app/Views/index.php
@@ -5,20 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?=base_url('styles/style.css')?>">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Zenith</title>
 </head>
 
 <body>
     <header>
-            <nav class="cabecera-de-navegacion">
-                <p class="sisisi">Zenith</p>
-            </nav>
+        Zenith
+        <nav class="nav-links">
+            <a href="">Categorias</a>
+            <a href="">Productos</a>
+            <a href="">Marcas</a>
+        </nav>
     </header>
-    <div class="contenido">
-        <h1>Welcome to Zenith</h1>
-        <p>Your gateway to excellence.</p>
-    <hr>
 </body>
 
 </html>
