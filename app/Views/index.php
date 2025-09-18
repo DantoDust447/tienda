52c6817e42280775ff92057eae3e1e8283417748
DantoDust447 <dantejosesanchez724@gmail.com>
2025-09-18 08:54:39 -0600

    add

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
-<html lang="en" data-bs-theme="dark">
+<html lang="es" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?=base_url('styles/style.css')?>">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <title>Zenith</title>
+    <title>Prime Suplements</title>
 </head>
 
 <body>
     <header>
-        Zenith
+        <div class="logo-container"><img src="<?=base_url('images/page-logo.jpeg')?>" alt="Logo de la tienda" width="75"
+                height="75">
+            Prime Suplements</div>
+
         <nav class="nav-links">
-            <a href="">Categorias</a>
-            <a href="">Productos</a>
-            <a href="">Marcas</a>
+            <a href="" class="links-style-header">Categorias</a>
+            <a href="" class="links-style-header">Marcas</a>
+            <a href="" class="links-style-header">Productos</a>
+            <a href="" class="links-style-header">Nosotros</a>
         </nav>
+        <div class="icons-header">
+            <i class="bi bi-cart4"> Carrito</i>
+            <i class="bi bi-search"> Buscar</i>
+        </div>
+        
     </header>
 </body>
 
